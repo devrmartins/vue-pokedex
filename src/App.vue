63281dad4224abbe4e303820f04673b8195f5306
Raffael Martins <rbarbosama@indracompany.com>
2020-12-22 14:51:09 -0300

    Criar componente Pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="pokemons">
+      <Pokemon
+        v-for="(pokemon, index) in pokemons"
+        :key="index"
+        :name="pokemon.name"
+        :url="pokemon.url"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-//import axios from "axios";
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
+
+const api = axios.create({
+  baseURL: "https://pokeapi.co/api/v2",
+});
 export default {
   name: "App",
+  components: {
+    Pokemon,
+  },
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: async function() {
+    let params = new URLSearchParams();
+    params.append("limit", 151);
+    params.append("offset", 0);
+
+    let res = await api.get(`/pokemon?${params.toString()}`);
+    this.pokemons = res.data.results;
+  },
 };
 </script>
 
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pokemon">
+    <h4>{{ num }} {{ name | upper }}</h4>
+    <p>{{ url }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  filters: {
+    upper: function(value) {
+      return value.toUpperCase();
+    },
+  },
+};
+</script>
+<style scoped></style>
