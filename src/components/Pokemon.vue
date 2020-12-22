1d2c390717996a61551d01d4793236fc42e0e8e8
Raffael Martins <rbarbosama@indracompany.com>
2020-12-22 17:44:39 -0300

    Trocando sprite

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,18 +1,27 @@
 <template>
-  <div id="pokemon" class="card">
-    <div class="card-image">
-      <figure class="image is-128x128">
-        <img :src="pokemon.sprites.front_default" alt="Placeholder image" />
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media-content">
-        <p class="title is-4 mb-3">{{ this.name }}</p>
-        <ul class="types">
-          <li class="type" v-for="(t, index) of pokemon.types" :key="index">
-            {{ t.type.name }}
-          </li>
-        </ul>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure
+          v-if="currentImg"
+          class="image is-128x128"
+          @mouseenter="changeSprite('back')"
+          @mouseleave="changeSprite('front')"
+        >
+          <img :src="currentImg" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4 mb-3">{{ this.name }}</p>
+            <ul class="types">
+              <li class="type" v-for="(t, index) of pokemon.types" :key="index">
+                {{ t.type.name }}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,12 +30,15 @@
 import api from "../api";
 export default {
   async created() {
+    this.isFront = true;
     let pokemon = await api.get(this.url);
     this.pokemon = pokemon.data;
-    console.log(pokemon);
+    this.currentImg = this.pokemon.sprites.front_default || "";
   },
   data() {
     return {
+      isFront: Boolean,
+      currentImg: String,
       pokemon: Object,
     };
   },
@@ -40,12 +52,24 @@ export default {
       return value.toUpperCase();
     },
   },
+  methods: {
+    changeSprite(current) {
+      if (current == "back") {
+        this.isFront = false;
+        this.currentImg = this.pokemon.sprites.back_default;
+      } else {
+        this.isFront = true;
+        this.currentImg = this.pokemon.sprites.front_default;
+      }
+    },
+  },
 };
 </script>
 <style scoped>
 #pokemon .image {
   margin: 0 auto;
 }
+
 .types {
   display: flex;
 }
